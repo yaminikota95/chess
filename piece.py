570318a5b1f48f302a0b9909e2611741c78d0cc6
Yamini Kota <62835085+yaminikota95@users.noreply.github.com>
2020-12-31 16:11:48 +0530

    Update piece.py

--- piece.py
+++ piece.py
@@ -3,6 +3,6 @@ class Piece:
   def __init__(self,color,pos_x,pos_y):
     self.curr_pos = (pos_x,pos_y)
     self.color = color
-  def move(self,new_pos):
-      self.curr_pos = new_pos
+  def move(self,new_pos_x,new_pos_y):
+      self.curr_pos = (new_pos_x,new_pos_y)
     
