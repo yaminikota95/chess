924294b804b12c9aa7bb4d528ec532bc9ff8f8c6
DILJITH K D <71268936+diljithkd@users.noreply.github.com>
2020-12-31 19:42:51 +0530

    Update pawn.py
    find_next_moves()

--- pawn.py
+++ pawn.py
@@ -1,4 +1,12 @@
 from config import *
 from piece import *
 class Pawn(Piece):
-    pass
+    def find_next_moves(self):
+        next_pos = []
+        a,b = self.curr_pos
+        if a<WIDTH-1:
+            next_pos.append((a+1,b))
+        if b<HEIGHT-1:
+            next_pos.append((a+1,b-1))
+            next_pos.append((a+1,b+1))
+        return next_pos
