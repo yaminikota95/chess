dcbccb7c6b93ea78da51e40b6ab606689db65bda
DILJITH K D <71268936+diljithkd@users.noreply.github.com>
2020-12-31 19:37:41 +0530

    Update knight.py
    find_next_moves()

--- knight.py
+++ knight.py
@@ -1,4 +1,14 @@
 from config import *
 from piece import *
 class Knight(Piece):
-    pass
+    def find_next_moves(self):
+        x_offset = [-1,-2,-2,-1,1,2,2,1]
+        y_offset = [-2,-1,1,2,2,1,-1,-2]
+        next_pos = []
+        for i,j in zip(x_offset, y_offset):
+            temp_i = self.curr_pos[0]-i
+            temp_j = self.curr_pos[1]-j
+            if temp_i < HEIGHT and temp_i >= 0 and\
+               temp_j < WIDTH and temp_j >= 0:
+                next_pos.append((temp_i, temp_j))
+        return next_pos
