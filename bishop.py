251ae596f2c9016d3261798df7aca46335fc092e
DILJITH K D <71268936+diljithkd@users.noreply.github.com>
2020-12-31 19:36:42 +0530

    Update bishop.py
    find_next_moves()

--- bishop.py
+++ bishop.py
@@ -1,4 +1,30 @@
 from config import *
 from piece import *
 class Bishop(Piece):
-    pass
+    def find_next_moves(self):
+        next_pos = []
+        a,b = self.curr_pos
+        while a>=0 and b>=0:
+            if a!= self.curr_pos[0] and b!=self.curr_pos[1]:
+                next_pos.append((a,b))
+            a = a-1
+            b = b-1
+        a,b = self.curr_pos
+        while a>=0 and b<HEIGHT:
+            if a!= self.curr_pos[0] and b!=self.curr_pos[1]:
+                next_pos.append((a,b))
+            a = a-1
+            b = b+1
+        a,b = self.curr_pos
+        while a<WIDTH and b>=0:
+            if a!= self.curr_pos[0] and b!=self.curr_pos[1]:
+                next_pos.append((a,b))
+            a = a+1
+            b = b-1
+        a,b = self.curr_pos
+        while a<WIDTH and b<HEIGHT:
+            if a!= self.curr_pos[0] and b!=self.curr_pos[1]:
+                next_pos.append((a,b))
+            a = a+1
+            b = b+1
+        return next_pos
