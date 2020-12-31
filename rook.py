12d95b3b468b00ecc1f502ac98d202230ca89c2b
Yamini Kota <62835085+yaminikota95@users.noreply.github.com>
2020-12-31 17:06:03 +0530

    find_next_moves function

--- rook.py
+++ rook.py
@@ -1,4 +1,13 @@
 from config import *
 from piece import *
 class Rook(Piece):
-    pass
+    def find_next_moves(self):
+        delta = [-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7]
+        poss = []
+        for i in delta:
+            if(self.curr_pos[0] + i >=0 and self.curr_pos[0] + i <8):
+                poss.append((self.curr_pos[0]+i,self.curr_pos[1]))
+        for i in delta:
+            if(self.curr_pos[1] + i >=0 and self.curr_pos[1] + i <8):
+                poss.append((self.curr_pos[0],self.curr_pos[1] +i))
+        return poss
