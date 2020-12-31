076866a7a605b81a3ae05867aabdd5d1b5299d97
Yamini Kota <62835085+yaminikota95@users.noreply.github.com>
2020-12-31 17:04:47 +0530

    find_next_moves function

--- queen.py
+++ queen.py
@@ -1,4 +1,11 @@
 from config import *
 from piece import *
 class Queen(Piece):
-    pass
+    def find_next_moves(self):
+        delta = [-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7]
+        poss = []
+        for i in delta:
+            for j in delta:
+                if(not(i==0 and j==0) and (i==j or i+j == 0 or i==0 or j==0) and self.curr_pos[0] + i >=0 and self.curr_pos[0] + i <8 and self.curr_pos[1] + j >=0 and self.curr_pos[1] + j <8):
+                    poss.append((self.curr_pos[0] +i , self.curr_pos[1]+j ))            
+        return poss
