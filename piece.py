cc8e955a40632073492838875f57905dc2389329
DILJITH K D <71268936+diljithkd@users.noreply.github.com>
2020-12-31 16:14:14 +0530

    Update piece.py

--- piece.py
+++ piece.py
@@ -4,5 +4,5 @@ class Piece:
     self.curr_pos = (pos_x,pos_y)
     self.color = color
   def move(self,new_pos_x,new_pos_y):
-      self.curr_pos = (new_pos_x,new_pos_y)
+    self.curr_pos = (new_pos_x,new_pos_y)
     
