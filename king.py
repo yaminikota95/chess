6e332a48418b8e7a9321a10bddf05e32d676d758
Yamini Kota <62835085+yaminikota95@users.noreply.github.com>
2020-12-31 17:03:13 +0530

    find_next_moves function

--- king.py
+++ king.py
@@ -1,4 +1,11 @@
 from config import *
 from piece import *
 class King(Piece):
-    pass
+    def find_next_moves(self):
+        delta = [-1,0,1]
+        poss = []
+        for i in delta:
+            for j in delta:
+                if(not(i==0 and j==0) and self.curr_pos[0] + i >=0 and self.curr_pos[0] + i <8 and self.curr_pos[1] + j >=0 and self.curr_pos[1] + j <8):
+                    poss.append((self.curr_pos[0] +i , self.curr_pos[1]+j ))
+        return poss
